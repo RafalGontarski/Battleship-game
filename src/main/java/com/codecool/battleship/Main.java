343a16183db257e67390215621f59af496e7a30c
RafalGontarski <gontarskirafal@gmail.com>
2022-12-14 15:41:02 +0100

    Size of ships stack works. 0.8v

--- src/main/java/com/codecool/battleship/Main.java
+++ src/main/java/com/codecool/battleship/Main.java
@@ -3,6 +3,7 @@ package com.codecool.battleship;
 import com.codecool.battleship.game.Battleship;
 
 public class Main {
+
     public static void main(String[] args) {
         Battleship battleship = new Battleship();
         battleship.start();

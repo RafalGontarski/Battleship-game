7ee534667bd1e296a3f9a00995b8f33d9ac6c374
RafalGontarski <gontarskirafal@gmail.com>
2022-12-16 11:52:46 +0100

    Refactor. 0.1v

--- src/main/java/com/codecool/battleship/Main.java
+++ src/main/java/com/codecool/battleship/Main.java
@@ -3,7 +3,6 @@ package com.codecool.battleship;
 import com.codecool.battleship.game.Battleship;
 
 public class Main {
-
     public static void main(String[] args) {
         Battleship battleship = new Battleship();
         battleship.start();
